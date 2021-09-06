0309cf2f8f6e80d846a8e9777073f7fa50014432
priyamjasani91 <86943907+priyamjasani91@users.noreply.github.com>
2021-09-06 19:11:34 +0530

    First Commit

--- src/main/java/guru/springframework/RecipeAppApplication.java
+++ src/main/java/guru/springframework/RecipeAppApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class RecipeAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+		SpringApplication.run(RecipeAppApplication.class, args);
 	}
 }
--- src/test/java/guru/springframework/RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/RecipeAppApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5RecipeAppApplicationTests {
+public class RecipeAppApplicationTests {
 
 	@Test
 	public void contextLoads() {
